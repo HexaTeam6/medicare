4b7b91d34fc85ad618ab674c45563d483e565416
HexaTeam6 <abdurrachmanwahed@gmail.com>
2022-05-05 21:54:29 +0700

    Initial Commit

---
+++ medicare/medicareApp.swift
@@ -0,0 +1,17 @@
+//
+//  medicareApp.swift
+//  medicare
+//
+//  Created by Abdur Rachman Wahed on 05/05/22.
+//
+
+import SwiftUI
+
+@main
+struct medicareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
